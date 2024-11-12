a5a83e90d156071dd17b20dad97a6f81a0c54583
ayanprime <86917904+ayanprime@users.noreply.github.com>
2024-11-12 16:31:54 -0500

    Update variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -1,21 +1,21 @@
-#include "main.h"
+#include "variable.hpp" // Only include variable.hpp, no main.h
 
 vex::timer myTimer;
-double dt;
-double odomR;
-double odomL;
-double etR;
-double etL;
-double RKp;
-double LKp;
-double dis;
-double InewR;
-double IoldR = 0;
-double InewL;
-double IoldL = 0;
-int elapsedTime = myTimer.time(msec);
-float Ki = 1;
-double PR;
-double PL;
-double IR;
-double IL;
+double dt = 0.0;
+double odomR = 0.0;
+double odomL = 0.0;
+double etR = 0.0;
+double etL = 0.0;
+double RKp = 0.0;
+double LKp = 0.0;
+double dis = 0.0;
+double InewR = 0.0;
+double IoldR = 0.0;
+double InewL = 0.0;
+double IoldL = 0.0;
+int elapsedTime = myTimer.time(vex::timeUnits (msec));
+float Ki = 1.0;
+double PR = 0.0;
+double PL = 0.0;
+double IR = 0.0;
+double IL = 0.0;
